f8886d5d87d92cfd091d7f388eef25cde13e813b
MUSHARIB-97 <mushariburrab9@gmail.com>
2026-03-26 04:53:45 +0500

    optimization

--- src/components/UserProfileLogo.tsx
+++ src/components/UserProfileLogo.tsx
@@ -3,7 +3,11 @@ import IMAGES from "../assets/images";
 function UserProfileLogo() {
   return (
     <div className="w-11 h-11 border-2 border-primary rounded-full overflow-hidden">
-      <img src={IMAGES.profile} alt="User profile" className="w-full h-full object-cover" />
+      <img
+        src={IMAGES.profile}
+        alt="User profile"
+        className="w-full h-full object-cover"
+      />
     </div>
   );
 }
